61ca94ff2a4e9a0fba6eb9779a01f9cd36119dc2
bondan sigit <88963945+bondsiGit@users.noreply.github.com>
2023-04-02 12:53:04 +0700

    after debugging

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const app = express();
 // methode dalam routing
 //app.methode(path, handler);
 
 
-const userRoutes = require('./routes/user.js')
+const userRoutes = require('./routes/user.js');
 
-const middlewareLogReq = require('./middleware/logs')
+const middlewareLogReq = require('./middleware/logs');
+
+// Create the connection pool. The pool-specific settings are the defaults
+const dbPool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'express_mysql'
+  });
 
 app.use(middlewareLogReq);
 
@@ -15,7 +24,20 @@ app.use(express.json())
 
 app.use("/user", userRoutes);
 
-
+app.use('/', (req, res) => {
+    dbPool.execute('SELECT * FROM user', (err, rows) => {
+        if(err){
+            console.log(err)
+            return res.json({
+                message: 'connection to db is failed...',
+            })
+        } 
+        return res.json({
+            message: 'connection to db is okey...',
+            data: rows,
+        })
+    })
+});
 
 app.listen(4000, () => {
     console.log('server is running...')
